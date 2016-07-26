67c320b28d69a9839c2ee014b986ed23d3cdcfb1
Chip Howell <chip.howell@gmail.com>
2016-07-25 23:59:23 -0700

    added fault protection

--- Project1/Grader.cpp
+++ Project1/Grader.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include "graderFunc.cpp"
 #include "grader.h"
 
 using namespace std;
@@ -9,21 +8,48 @@ int main()
 	int i;
 	int c;
 	bool check = true;
-		
+	bool checkTwo;
+	bool checkThree;
+
 	while (check == true)
 	{
-		cout << "Enter numerical score: ";
-		cin >> i;
+		checkTwo = true;
+		checkThree = true;
+		
 
-		if (i >= 0 && i <= 100)
-			grader(i);
+		while (checkThree == true)
+		{
+			cout << "Enter numerical score: ";
+			cin >> i;
 
-		cout << "Enter 1 to check another grade or 2 to quit: ";
-		cin >> c;
-		if (c == 1)
-			check = true;
-		else
-			check = false;
+			if (i >= 0 && i <= 100)
+			{
+				grader(i);
+				checkThree = false;
+			}
+
+			else
+				cout << "Invalid score. Input again. " << endl;
+		}
+
+			while (checkTwo == true)
+			{
+				cout << "Enter 1 to check another grade or 2 to quit: ";
+				cin >> c;
+				if (c == 1)
+				{
+					check = true;
+					checkTwo = false;
+				}
+
+				else if (c == 2)
+				{
+					check = false;
+					checkTwo = false;
+				}
+				else
+					cout << "Invalid choice." << endl;
+			}
 	}
 
 	system("pause");
